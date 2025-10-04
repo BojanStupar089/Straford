2bb64601a03bb09ddd14693bdda5e0eb595bec00
BojanStupar089 <bojanstupar089@gmail.com>
2025-10-04 16:44:25 +0200

    init..

---
+++ StrafordWebApi/StrafordApi/Controllers/UsersController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace StrafordApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsersController : Controller
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Zvezdaleeeee");
+        }
+    }
+}
